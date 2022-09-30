55d4040a0604e929e26760c7cb56c2c9f22ff8ee
Ekate Razuyeva <25-ky-ky08@mail.ru>
2022-09-30 13:30:58 +0300

    09

---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import App from "./App";
+
+function Header() {
+    return (
+        <div>
+            <a href=" ">Home</a>
+            <a href=" ">New Feeds</a>
+            <a href=" ">Messages</a>
+        </div>
+    )
+}
+
+export default Header;
