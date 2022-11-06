186582214923e73ce2a4d6c476709b6cec8810bc
Ekate Razuyeva <25-ky-ky08@mail.ru>
2022-11-06 11:57:05 +0300

    12-split-into-components

---
+++ src/componets/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Header(){
+    return(
+        <header className={'header'}>
+            <img
+                src="https://cdn.dribbble.com/userupload/3060297/file/original-eb1473537fcbd41cea9c93c882662d29.jpg?resize=400x0"
+                alt="logo"/>
+        </header>
+    )
+}
---
+++ src/componets/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export function Profile(){
+    return(
+        <div className={'content'}>
+            <img
+                src="https://celes.club/uploads/posts/2022-04/1651139912_1-celes-club-p-peizazh-v-perspektive-priroda-krasivo-foto-3.jpg"
+                alt=""/>
+            <div>
+                ava + description
+            </div>
+            <div>
+                my posts
+            </div>
+            <div>
+                New post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    )
+}
